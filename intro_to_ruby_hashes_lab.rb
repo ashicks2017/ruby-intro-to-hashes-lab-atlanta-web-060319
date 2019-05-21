ff601da3051130e63a0cf7d48f11bc4be02c9aaf
aeisha simmons <ashicks2017@outlook.com>
2019-05-21 02:44:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,14 +1,19 @@
 def new_hash
-
+  book = {}
+  return book
 end
 
 def actor
-
+  actor = {name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
-
+  monopoly = {}
+  monopoly = {
+   :railroads => {}
+   
+   }
+   
 end
 
 def monopoly_with_second_tier
